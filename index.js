39f81f91adc3f808201945eca49a30f16a205f40
try4code <sivagmk12@gmail.com>
2018-11-11 13:39:25 -0500

    added the REST api

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const _ = require('underscore');
+const Joi = require('joi');
 const express = require('express'); 
 const app = express();
+
+app.use(express.json());
 
 var result = _.contains([1,12,4], 3);
 console.log(result);
@@ -12,7 +15,10 @@ app.get('/aoi', (req,res) => {
 });
 
 app.get('/servers/:year', (req,res)=>{
-    res.send(req.params);
+    res.write(`${JSON.stringify(req.params)}`);
+    res.write(`${JSON.stringify(req.query)}`);
+    res.end();
+    // res.send(req.params);
     // res.send(req.query);
 })
 
@@ -22,6 +28,22 @@ app.get('/rices/:id',(req,res) => {
     res.send(rice);
 })
 
-const port = process.env.PORT || 3000;  
+app.post('/rices',(req,res)=>{
+    const schema = { name: Joi.string().min(3).required()};
+    const result=Joi.validate(req.body, schema);
+    console.log("result from Joi: ",result)
+    if (result.error) { 
+        res.status(400).send(result.error.details[0].message); 
+        return; }
+    const item = {
+        id: rices.length + 1,
+        name: req.body.name
+    };
+    rices.push(item);
+    console.log(rices);
+    res.status(200).send(`Suceessfully updated ${JSON.stringify(item)}`);
+})
 
+
+const port = process.env.PORT || 3000;  
 app.listen(port, () => console.log(`Listenning on port ${port}`));
